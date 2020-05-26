a388115a2109f9e7d1147e8b8e2d995d83823498
Manish Poduval <manishpoduval@hotmail.com>
2020-05-26 15:15:33 +0530

    Express

--- index.js
+++ index.js
@@ -1,8 +1,68 @@
 console.log('Hey, Node is working!');
 
-let {mentor, student, sumFunc} = require('./data.js')
-let bootcampName = require('./bootcamp') 
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Heyyyyy Works')
+})
 
-console.log(sumFunc(4, 5));
+app.get('/home', function (req, res) {
+    res.send('I\'m at home');
+})
 
-console.log(bootcampName)
+app.get('/bootcamp', function (req, res) {
+    res.send('Node is fun!!!');
+})
+
+app.get('/tuesday', function (req, res) {
+    res.send('Heyyyyy its Tuesday')
+})
+ 
+app.listen(3000, '127.0.0.1', () => {
+    console.log(`Server running at http://127.0.0.1:3000/`);
+})
+
+
+
+
+
+
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.end('Hello World Manish');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+
+// const chalk = require('chalk');
+// console.log(chalk.green(
+//     'I am a green line ' +
+//     chalk.blue.underline.bold('with a blue substring') +
+//     ' that becomes green again!'
+// ));
+
+// let {mentor, student, sumFunc} = require('./data.js')
+// let bootcampName = require('./bootcamp') 
+
+// console.log(sumFunc(4, 5));
+
+// console.log(bootcampName)
+
+
+// var faker = require('faker');
+
+// for (let i=0 ; i< 10; i++) {
+//     var randomProdName = faker.commerce.productName(); 
+//     var randomPrice = faker.commerce.price(); 
+//     console.log(randomProdName+ ' $' + randomPrice);
+// }
