86f1d80942ba3b9f95a286302dd1f35687196b79
Manish Poduval <manishpoduval@hotmail.com>
2020-05-26 13:03:49 +0530

    starter

---
+++ data.js
@@ -0,0 +1,15 @@
+const student = 'Manish';
+const mentor = 'Jorge';
+
+function sum(a, b){
+    return a+b;
+}
+
+let myObj =  {
+    'student': student,
+    'mentor': mentor,
+    'sumFunc': sum,
+}
+
+
+module.exports = myObj;
---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Hey, Node is working!');
+
+let {mentor, student, sumFunc} = require('./data.js')
+let bootcampName = require('./bootcamp') 
+
+console.log(sumFunc(4, 5));
+
+console.log(bootcampName)
